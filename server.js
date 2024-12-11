97f3ef625941da22831508570a9ba1387c21e4e5
epelix <90451715+eemeliHuotari@users.noreply.github.com>
2024-12-11 18:55:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
-// OpenShift sample Node application
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-
-Object.assign = require('object-assign');
-
+    
+Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'));
+app.use(morgan('combined'))
 
-
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    
 app.get('/', function (req, res) {
-    res.render('index.html', { pageCountMessage: null });
+    res.render('index.html', { pageCountMessage : null});
 });
-
 
 app.get('/pagecount', function (req, res) {
     res.send('{ pageCount: -1 }');
+  }
 });
 
-
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something bad happened!');
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
 });
 
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
 
-app.listen(port, ip, function () {
-    console.log('Server running on http://%s:%s', ip, port);
-});
-
-module.exports = app;
+module.exports = app ;
